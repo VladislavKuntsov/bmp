3f92ee043185c87f940f0cdd075cbc39392b646b
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-10-11 19:15:17 +0300

    Initial start

--- src/Components/App/app.js
+++ src/Components/App/app.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Header from "../Header/header";
-import Menu from "../Menu/menu"
-import SignIn from "../Sign-in/sign-in";
-import GenderSelect from "../Gender-select/gender-select"
-import ProductList from "../Product-list/product-list";
-import Banners from "../Banners/banners";
-import BlogPreview from "../Blog-preview/blog-preview";
-import Footer from "../Footer/footer";
+// import Header from "../Header/header";
+// import Menu from "../Menu/menu"
+// import SignIn from "../Sign-in/sign-in";
+// import GenderSelect from "../Gender-select/gender-select"
+// import ProductList from "../Product-list/product-list";
+// import Banners from "../Banners/banners";
+// import BlogPreview from "../Blog-preview/blog-preview";
+// import Footer from "../Footer/footer";
+import Test from "../Tests/test";
 
 const App = () => (
-    <div>
-        <Router>
-            <Route path="/misto/sign-in" exact component={SignIn} />
-            <Route path={["/", "/misto"]} exact  component={Header} />
-            <Route path={["/", "/misto"]} exact  component={Menu} />
-            <Route path={["/", "/misto"]} exact  component={GenderSelect} />
-            <Route path={["/", "/misto"]} exact  component={ProductList} />
-            <Route path={["/", "/misto"]} exact  component={ProductList} />
-            <Route path={["/", "/misto"]} exact  component={Banners} />
-            <Route path={["/", "/misto"]} exact  component={BlogPreview} />
-            <Route path={["/", "/misto"]} exact  component={Footer} />
-            
-        </Router> 
-    </div>
-)
+        <div>
+            <Router>
+                <Route path={["/", "/misto"]} exact component={Test} />
+                {/* <Route path="/misto/sign-in" exact component={SignIn} />
+                <Route path={["/", "/misto"]} exact  component={Header} />
+                <Route path={["/", "/misto"]} exact  component={Menu} />
+                <Route path={["/", "/misto"]} exact  component={GenderSelect} />
+                <Route path={["/", "/misto"]} exact  component={ProductList} />
+                <Route path={["/", "/misto"]} exact  component={ProductList} />
+                <Route path={["/", "/misto"]} exact  component={Banners} />
+                <Route path={["/", "/misto"]} exact  component={BlogPreview} />
+                <Route path={["/", "/misto"]} exact  component={Footer} /> */}
+                
+            </Router> 
+        </div>          
+      )
 
 export default App;
---
+++ src/Components/Tests/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+
+    const obj = {}
+
+    console.log(obj.prototype === Object.prototype)
+
+    return (
+        <div>Привет</div>
+    )
+}      
+
+export default Menu;
